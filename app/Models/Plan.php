2fa144b7c059d201e3855fcdb97d78381823d8ca
RasonYudha4 <rasonyudha921.com>
2024-10-14 17:55:58 +0700

    Creating OneToMany relationships between plan and user collections model

---
+++ app/Models/Plan.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\Laravel\Eloquent\Model;
+
+class Plan extends Model
+{
+    use HasFactory;
+
+    protected $collection = 'plans';
+
+    protected $fillable = [
+        'title',
+        'user_id'
+    ];
+
+    public function user() : BelongsTo 
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function plans() : HasMany
+    {
+        return $this->hasMany(Plan::class);
+    }
 }
