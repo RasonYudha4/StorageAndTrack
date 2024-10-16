de0abf3f1f36b37e20d062f5ad0cbdcef50fb78f
rasonyudha4 <rasonyudha921@gmail.com>
2024-10-16 07:07:51 +0700

    Completing navbar
    - readjust the ui of the username dropdown

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -55,7 +55,7 @@ export default function Authenticated({
                                         <span className="inline-flex rounded-md">
                                             <button
                                                 type="button"
-                                                className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
+                                                className="inline-flex items-center rounded-md border border-transparent bg-[#a449d1] px-3 py-2 text-sm font-medium leading-4 text-white transition duration-150 ease-in-out hover:text-[#540d87] focus:outline-none"
                                             >
                                                 {user.name}
 
