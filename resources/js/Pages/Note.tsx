83901b39c411f2ae0f1cf02560e12f50b4174f1b
rasonyudha4 <rasonyudha921@gmail.com>
2024-10-18 13:33:00 +0700

    Ui adjustment for notes page

--- resources/js/Pages/Note.tsx
+++ resources/js/Pages/Note.tsx
@@ -10,15 +10,15 @@ interface NoteProps {
     content: string,
     writtenDate: string
 }
-export default function Note({title, content, writtenDate}: NoteProps) {
+export default function Note({ title, content, writtenDate }: NoteProps) {
     return (
         <AuthenticatedLayout>
             <Head title="Tasks" />
 
             <div className=' relative bg-[#540d87] bg-opacity-70 rounded-xl text-white mx-auto mt-8 max-w-[85rem] min-h-[40rem] px-4 py-8 sm:px-6 lg:px-8'>
                 <div className=' grid grid-cols-6 gap-5 my-auto bg-transparent h-[36rem] w-full'>
-                    
-                    <div className=' border-r-4 border-[#a449d1] p-2 flex flex-col '>
+
+                    <div className=' border-r-4 border-[#a449d1] p-2 flex flex-col row-span-2'>
                         <div className=' bg-[#a449d1] w-28 h-14 rounded-lg my-4 flex hover:bg-opacity-40 hover:cursor-pointer'>
                             <img src={Plus} alt="" className=' h-6 w-6 my-auto ml-4' />
                             <div className=' my-auto ml-4 font-semibold'>New</div>
@@ -36,10 +36,33 @@ export default function Note({title, content, writtenDate}: NoteProps) {
                             <p className=' text-[#540d87] my-auto font-bold ml-3 text-sm'>Folder 3</p>
                         </div>
                     </div>
-                    <div className=' py-6'>
-                        {content}
+                    
+                        <div className=' bg-[#a449d1] w-full h-10 rounded-lg mt-4 flex hover:bg-opacity-40 focus:bg-opacity-40'>
+                            <img src={Folder} alt="" className=' h-4 w-4 my-auto ml-3' />
+                            <p className=' text-[#540d87] my-auto font-bold ml-3 text-sm'>Folder 3</p>
+                        </div>
+                        <div className=' bg-[#a449d1] w-full h-10 rounded-lg mt-4 flex hover:bg-opacity-40 focus:bg-opacity-40'>
+                            <img src={Folder} alt="" className=' h-4 w-4 my-auto ml-3' />
+                            <p className=' text-[#540d87] my-auto font-bold ml-3 text-sm'>Folder 3</p>
+                        </div>
+                        <div className=' bg-[#a449d1] w-full h-10 rounded-lg mt-4 flex hover:bg-opacity-40 focus:bg-opacity-40'>
+                            <img src={Folder} alt="" className=' h-4 w-4 my-auto ml-3' />
+                            <p className=' text-[#540d87] my-auto font-bold ml-3 text-sm'>Folder 3</p>
+                        </div>
+                        <div className=' bg-[#a449d1] w-full h-10 rounded-lg mt-4 flex hover:bg-opacity-40 focus:bg-opacity-40'>
+                            <img src={Folder} alt="" className=' h-4 w-4 my-auto ml-3' />
+                            <p className=' text-[#540d87] my-auto font-bold ml-3 text-sm'>Folder 3</p>
+                        </div>
+                        <div className=' bg-[#a449d1] w-full h-10 rounded-lg mt-4 flex hover:bg-opacity-40 focus:bg-opacity-40'>
+                            <img src={Folder} alt="" className=' h-4 w-4 my-auto ml-3' />
+                            <p className=' text-[#540d87] my-auto font-bold ml-3 text-sm'>Folder 3</p>
+                        </div>
+                        <div className=' bg-[#a449d1] w-full h-10 rounded-lg mt-4 flex hover:bg-opacity-40 focus:bg-opacity-40'>
+                            <img src={Folder} alt="" className=' h-4 w-4 my-auto ml-3' />
+                            <p className=' text-[#540d87] my-auto font-bold ml-3 text-sm'>Folder 3</p>
+                        </div>
+                        
                     </div>
-                </div>
                 <AddButton className='absolute bottom-10 right-10 h-20 w-20 rounded-full'><img src={Plus} alt="" /></AddButton>
             </div>
         </AuthenticatedLayout>
