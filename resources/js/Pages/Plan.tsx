df47589e1e7a860ec0098bcc895ceb5ad650f733
rasonyudha4 <rasonyudha921@gmail.com>
2024-10-19 08:16:50 +0700

    Plan page ui design done

--- resources/js/Pages/Plan.tsx
+++ resources/js/Pages/Plan.tsx
@@ -1,12 +1,30 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import AddButton from '@/Components/AddButton';
 import { Head } from '@inertiajs/react';
+import Plus from '../../../public/images/plus.png';
+import Check from '../../../public/images/check.png';
+import CheckButton from '@/Components/CheckButton';
 
 export default function Plan() {
     return (
         <AuthenticatedLayout>
-            <Head title="Plan" />
+            <Head title="Tasks" />
 
-            
+            <div className=' relative bg-[#540d87] rounded-xl opacity-70 mx-auto mt-8 max-w-[85rem] min-h-[40rem] px-4 py-8 sm:px-6 lg:px-8'>
+            <div className=' my-auto bg-transparent h-[36rem] overflow-y-auto'>
+                <div className=' bg-[#a449d1] w-full h-12 rounded-3xl mb-8 '></div>
+                <div className=' bg-[#a449d1] w-full h-12 rounded-3xl mb-8 '></div>
+                <div className=' bg-[#a449d1] w-full h-12 rounded-3xl mb-8 '></div>
+                <div className=' bg-[#a449d1] w-full h-12 rounded-3xl mb-8 '></div>
+                <div className=' bg-[#a449d1] w-full h-12 rounded-3xl mb-8 '></div>
+                <div className=' bg-[#a449d1] w-full h-12 rounded-3xl mb-8 '></div>
+                <div className=' bg-[#a449d1] w-full h-12 rounded-3xl mb-8 '></div>
+                <div className=' bg-[#a449d1] w-full h-12 rounded-3xl mb-8 '></div>
+                <div className=' bg-[#a449d1] w-full h-12 rounded-3xl mb-8 '></div>
+                <AddButton className='absolute bottom-10 right-10 h-20 w-20 rounded-full'><img src={Plus} alt="" /></AddButton>
+                <CheckButton className='absolute bottom-10 right-40 h-20 w-20 rounded-full'><img src={Check} alt="" /></CheckButton>
+            </div>
+            </div>
         </AuthenticatedLayout>
     );
 }
--- resources/js/Pages/Task.tsx
+++ resources/js/Pages/Task.tsx
@@ -12,34 +12,34 @@ interface NoteProps {
     writtenDate: string
 }
 
-export default function Note({title, content, writtenDate}: NoteProps) {
+export default function Note({ title, content, writtenDate }: NoteProps) {
     return (
         <AuthenticatedLayout>
             <Head title="Note" />
 
             <div className=' relative bg-[#540d87] rounded-xl opacity-70 mx-auto mt-8 max-w-[85rem] min-h-[40rem] px-4 py-8 sm:px-6 lg:px-8'>
-            <div className=' my-auto bg-transparent h-[36rem] w-full'>
+                <div className=' my-auto bg-transparent h-[36rem] w-full'>
                     <div className=' flex justify-between '>
-                        <div className=' flex'>    
+                        <div className=' flex'>
                             <div className=' pr-4'>
-                               <p className=' font-extrabold text-3xl py-2'>{title}</p>
-                               <p className=' font-semibold text-lg '>{writtenDate}</p>
+                                <p className=' font-extrabold text-3xl py-2'>{title}</p>
+                                <p className=' font-semibold text-lg '>{writtenDate}</p>
                             </div>
                             <div className=' flex'>
-                                <div className=' bg-[#a449d1] rounded-full h-12 w-12 mt-5 ml-6 hover:bg-[#540d87]'><img src={Left} alt="" className=' h-6 w-6 m-3 '/></div>
-                                <div className=' bg-[#a449d1] rounded-full h-12 w-12 mt-5 ml-6 hover:bg-[#540d87]'><img src={Right} alt="" className=' h-6 w-6 m-3 '/></div>
+                                <div className=' bg-[#a449d1] rounded-full h-12 w-12 mt-5 ml-6 hover:bg-[#540d87]'><img src={Left} alt="" className=' h-6 w-6 m-3 ' /></div>
+                                <div className=' bg-[#a449d1] rounded-full h-12 w-12 mt-5 ml-6 hover:bg-[#540d87]'><img src={Right} alt="" className=' h-6 w-6 m-3 ' /></div>
                             </div>
                         </div>
                         <div className=' h-10 w-10 m-5 hover:opacity-80'>
-                            <img src={Trash} alt=""/>
+                            <img src={Trash} alt="" />
                         </div>
                     </div>
                     <div className=' border-b-4 border-[#540d87] p-2 '></div>
                     <div className=' py-6'>
                         {content}
                     </div>
-                <AddButton className='absolute bottom-10 right-10 h-20 w-20 rounded-full'><img src={Plus} alt="" /></AddButton>
-            </div>
+                    <AddButton className='absolute bottom-10 right-10 h-20 w-20 rounded-full'><img src={Plus} alt="" /></AddButton>
+                </div>
             </div>
         </AuthenticatedLayout>
     );
